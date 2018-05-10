3be05736722c924edb013c372bb40129f0101ccd
Conor Hamilton <conhamil19@gmail.com>
2018-05-10 07:06:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,10 +15,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if  text.match(/^[A-Z]/) && text.match(/[.,!]$/) 
-  return true
-else false
-end
+ text.match(/^[A-Z]/) && text.match(/[.,!]$/) 
+
 end
 
 def valid_phone_number?(phone)
